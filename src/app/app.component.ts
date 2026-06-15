5762a534cf0b51808cb64d63e7b199a79876fb66
VaishnaviRW <wadgaonkar7744@gmail.com>
2026-06-15 20:24:26 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TDformregistration';
+  @ViewChild('studentform') studentform!:NgForm
+genders: string[] = ['Male', 'Female'];
+  onSubmit(){
+    if(this.studentform.valid){
+      this.studentform.form.markAllAsTouched()
+    }
+    this.studentform.reset()
+  }
+onlyNumbers(event: KeyboardEvent) {
+  const key = event.key;
+
+  if (!/^[0-9]$/.test(key)) {
+    event.preventDefault();
+  }
+}
+
+onMobileInput(event: any) {
+  event.target.value = event.target.value.replace(/[^0-9]/g, '');
+}
+}
+
+
+
+
+
